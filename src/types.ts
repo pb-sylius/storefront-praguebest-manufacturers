4b53d64d564228b7bb2f5d0e707caa3fd2706159
Alexej Mrštík <a@mrstik.cz>
2023-01-01 23:04:49 +0100

    no message

--- src/types.ts
+++ src/types.ts
@@ -7,6 +7,7 @@ import {
   CustomQuery,
   FactoryParams
 } from "@vue-storefront/core";
+import { Product } from '@realtainment/sylius-api/src/types';
 
 export interface UseManufacturerErrors {
   search: Error;
@@ -18,6 +19,7 @@ export interface ManufacturerSearchParams {
   sort?: any;
   term?: any;
   filters?: any;
+  id?: string;
   [x: string]: any;
 }
 
@@ -26,11 +28,15 @@ export interface UseManufacturer<MANUFACTURERS, MANUFUACTURER_SEARCH_PARAMS, API
   loading: ComputedProperty<boolean>;
   error: ComputedProperty<UseManufacturerErrors>;
   search(params: ComposableFunctionArgs<MANUFUACTURER_SEARCH_PARAMS>): Promise<void>;
+  one(params: ComposableFunctionArgs<MANUFUACTURER_SEARCH_PARAMS>): Promise<void>;
+  all(params: ComposableFunctionArgs<MANUFUACTURER_SEARCH_PARAMS>): Promise<void>;
   [x: string]: any;
 }
 
 export interface UseManufacturerFactoryParams<MANUFACTURERS, MANUFUACTURER_SEARCH_PARAMS extends ManufacturerSearchParams, API extends PlatformApi = any> extends FactoryParams<API> {
   manufacturersSearch: (context: Context, params: MANUFUACTURER_SEARCH_PARAMS & { customQuery?: CustomQuery }) => Promise<MANUFACTURERS>;
+  manufacturersAll: (context: Context) => Promise<MANUFACTURERS>;
+  manufacturersOne: (context: Context, params: MANUFUACTURER_SEARCH_PARAMS & { customQuery?: CustomQuery }) => Promise<MANUFACTURERS>;
 }
 
 export type Manufacturer = Record<string, unknown>;
@@ -39,3 +45,12 @@ export type ManufacturerResponse = {
   data: Manufacturer[];
   total: number;
 };
+
+export interface ManufacturerGetters<MANUFACTURER> {
+  getName: (manufacturer: MANUFACTURER) => string;
+  getSlug: (manufacturer: MANUFACTURER) => string;
+  getCoverImage: (manufacturer: MANUFACTURER) => string;
+  getId: (manufacturer: MANUFACTURER) => string;
+  getProducts: (manufacturer: MANUFACTURER) => Product[];
+  [getterName: string]: any;
+}
